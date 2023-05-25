7ff3550808dda76e05cc204c1b62c172949d1707
Vyankatesh-2108 <vyankates_khetre@moderncoe.edu.in>
2023-05-26 04:42:26 +0530

    responsive admin dashboard

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import webapp2
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__),"index.html")
+        self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication([('/',MainPage)],debug=True)
